b6d5583923d5ba72e953f79037c5254f1336195c
nsvillegas@espe.edu.ec <nsvillegas@espe.edu.ec>
2025-07-11 16:45:34 -0500

    cors

---
+++ analisis/src/main/java/com/banquito/analisis/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.banquito.analisis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/")
+                .allowedOrigins("http://ec2-18-116-67-249.us-east-2.compute.amazonaws.com:4173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
